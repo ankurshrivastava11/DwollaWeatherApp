2d9d29ea279b50c65faa29a2f8176d2d9a7a5683
ankurshrivastava11 <ankur-shrivastava@uiowa.edu>
2017-06-01 01:44:26 -0500

    rest call

---
+++ test/org/dwolla/WeatherUnitTest.java
@@ -0,0 +1,71 @@
+package org.dwolla;
+
+import static org.mockito.Mockito.when;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class WeatherUnitTest {
+
+	@Mock
+	HttpServletRequest request;
+
+	@Mock
+	HttpServletResponse response;
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	
+	@Test
+	public void validCity() throws Exception {
+		
+		when(request.getParameter("location")).thenReturn("Chicago");
+		
+		JSONObject obj = new JSONObject();
+		obj.put("main", new JSONObject().put("temp", 56.28));
+
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+
+		when(response.getWriter()).thenReturn(pw);
+
+		new Weather().doGet(request, response);
+
+		String result = sw.getBuffer().toString().trim();
+
+		Assert.assertTrue(result.contains("Fahrenheit"));
+	}
+	
+	@Test
+	public void invalidCity() throws Exception {
+		
+		when(request.getParameter("location")).thenReturn("1");
+		
+		JSONObject obj = new JSONObject();
+		obj.put("main", new JSONObject().put("temp", 56.28));
+
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+
+		when(response.getWriter()).thenReturn(pw);
+
+		new Weather().doGet(request, response);
+
+		String result = sw.getBuffer().toString().trim();
+
+		Assert.assertFalse(result.contains("Fahrenheit"));
+	}
+}
